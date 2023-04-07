9ed21e8d499f361524f52cf409b362fc76cb45f5
miloszwierucki <milosz@wierucki.com>
2023-04-07 14:34:03 +0200

    feature: add more translation

--- src/locales/i18n.ts
+++ src/locales/i18n.ts
@@ -2,18 +2,21 @@ import { initReactI18next } from "react-i18next";
 import i18n from "i18next";
 
 import translationEN from "./en/translation.json";
+import projectsEN from "./en/projects.json";
 import translationPL from "./pl/translation.json";
+import projectsPL from "./pl/projects.json";
 
 const resources = {
   en: {
-    translation: translationEN,
+    translation: { ...translationEN, ...projectsEN },
   },
   pl: {
-    translation: translationPL,
+    translation: { ...translationPL, ...projectsPL },
   },
 };
 
 i18n.use(initReactI18next).init({
+  defaultNS: "translation",
   resources,
   lng: "en",
 
